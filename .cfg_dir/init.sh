6dfca493f09b614d1f14821e6e6e1c07a2ecc8db
Laurent Arthur <laurent.arthur75@gmail.com>
2020-10-26 02:36:08 +0100

    Add a config refresh alias

--- .cfg_dir/init.sh
+++ .cfg_dir/init.sh
@@ -1,5 +1,6 @@
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias ref="source ~/.cfg_dir/aliases/*"
 config config --local status.showUntrackedFiles no
 
-source ~/.cfg_dir/aliases/*
+ref
