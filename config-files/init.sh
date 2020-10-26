cd4443d1ddff443e85b47341d885a70d0bddc154
Laurent Arthur <laurent.arthur75@gmail.com>
2020-10-26 02:36:08 +0100

    Set SHELL_NAME on first init script

--- config-files/init.sh
+++ config-files/init.sh
@@ -3,6 +3,11 @@ CONFIG_FILES_DIR=~/.config/config-files
 alias config='/usr/bin/git --git-dir=$HOME/.config/.git --work-tree=$HOME/.config'
 alias ref="source $CONFIG_FILES_DIR/init.sh"
 
+case $SHELL in
+    */bash) SHELL_NAME="bash";;
+    */zsh) SHELL_NAME="zsh";;
+esac
+
 source_files () {
     for f in $1/*
     do
