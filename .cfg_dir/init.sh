a3dc87030441db52a02bfa55e0e400f5ff53a422
Laurent Arthur <laurent.arthur75@gmail.com>
2020-10-26 02:36:08 +0100

    Fix cfg_dir reload alias, and add global_gitignore file

--- .cfg_dir/init.sh
+++ .cfg_dir/init.sh
@@ -1,6 +1,11 @@
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias ref="source ~/.cfg_dir/aliases/*"
+
+source ~/.cfg_dir/aliases/*
+
 config config --local status.showUntrackedFiles no
 
-ref
+git config --global core.excludesfile '~/.cfg_dir/config/.global_gitignore'
+
+
+alias ref="source ~/.cfg_dir/init.sh"
